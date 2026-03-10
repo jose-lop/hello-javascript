abc6bb6ba3c97643c690596afbe4a3393b263938
jose-lop <j.g.antoniolopez1987@gmail.com>
2026-03-10 07:15:13 -0500

    Basic/17-functions-exercises.js

--- Basic/17-functions-exercises.js
+++ Basic/17-functions-exercises.js
@@ -6,14 +6,58 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=14146
 // NOTA: Explora diferentes sintaxis de funciones para resolver los ejercicios
 
 // 1. Crea una función que reciba dos números y devuelva su suma
+console.log("1. Crea una función que reciba dos números y devuelva su suma");
+function sumar(a, b) {
+  return a + b;
+}
+console.log(sumar(5, 3));
 
 // 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
+console.log(
+  "2. Crea una función que reciba un array de números y devuelva el mayor de ellos",
+);
+function encontrarMayor(numeros) {
+  let mayor = numeros[0];
+  for (let i = 1; i < numeros.length; i++) {
+    if (numeros[i] > mayor) {
+      mayor = numeros[i];
+    }
+  }
+  return mayor;
+}
+console.log(encontrarMayor([3, 7, 2, 9, 5]));
 
 // 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
+console.log(
+  "3. Crea una función que reciba un string y devuelva el número de vocales que contiene",
+);
+function contarVocales(texto) {
+  const vocales = "aeiouAEIOU";
+  let contadorVocales = 0;
+  for (let i = 0; i < texto.length; i++) {
+    if (vocales.includes(texto[i])) {
+      contadorVocales++;
+    }
+  }
+  return contadorVocales;
+}
+console.log(contarVocales("Hola Mundo"));
 
 // 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
+console.log(
+  "4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas",
+);
+function convertirMayusculas(strings) {
+  const mayúsculas = [];
+  for (let i = 0; i < strings.length; i++) {
+    mayúsculas.push(strings[i].toUpperCase());
+  }
+  return mayúsculas;
+}
+console.log(convertirMayusculas(["hola", "mundo", "javascript"]));
 
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
+console.log(
 
 // 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
 
