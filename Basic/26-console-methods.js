747ef36298f5550205bed7db0d04958bef4ab5ca
jose-lop <j.g.antoniolopez1987@gmail.com>
2026-05-26 18:58:04 -0500

    Primer commit 27 console.log

--- Basic/26-console-methods.js
+++ Basic/26-console-methods.js
@@ -6,91 +6,93 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=20444
 // Console
 
 // log
+import chalk from "chalk";
 
-console.log("¡Hola, JavaScript!")
+console.log(chalk.yellow("Advertencia"));
+console.log(chalk.red("Error"));
+console.log(chalk.green("Correcto"));
+
+console.log("¡Hola, JavaScript!");
 
 // error
 
-console.error("Este es un mensaje de error.")
-console.error("Error al conectarse a la base de datos: ", new Error("Conexión fallida."))
+console.error("Este es un mensaje de error.");
+console.error(
+  "Error al conectarse a la base de datos: ",
+  new Error("Conexión fallida."),
+);
 
 // warn
 
-console.warn("Este es un mensaje de advertencia.")
+console.warn("Este es un mensaje de advertencia.");
 
 // info
 
-console.info("Este es un mensaje de información adicional.")
+console.info("Este es un mensaje de información adicional.");
 
 // table
 
 let data = [
-    ["Brais", 37],
-    ["Sara", 21]
-]
+  ["Brais", 37],
+  ["Sara", 21],
+];
 
-console.table(data)
+console.table(data);
 
 data = [
-    { name: "Brais", age: 37 },
-    { name: "Sara", age: 21 }
-]
+  { name: "Brais", age: 37 },
+  { name: "Sara", age: 21 },
+];
 
-console.table(data)
+console.table(data);
 
 // group
 
-console.group("Usuario:")
-console.log("Nombre: Brais")
-console.log("Edad: 37")
-console.groupEnd()
+console.group("Usuario:");
+console.log("Nombre: Brais");
+console.log("Edad: 37");
+console.groupEnd();
 
 // time
 
-console.time("Tiempo de ejecución 2")
+console.time("Tiempo de ejecución 2");
 
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 10000; i++) {}
 
-}
+console.time("Tiempo de ejecución 1");
 
-console.time("Tiempo de ejecución 1")
+for (let i = 0; i < 10000; i++) {}
 
-for (let i = 0; i < 10000; i++) {
+console.timeEnd("Tiempo de ejecución 2");
 
-}
+for (let i = 0; i < 10000; i++) {}
 
-console.timeEnd("Tiempo de ejecución 2")
-
-for (let i = 0; i < 10000; i++) {
-
-}
-
-console.timeEnd("Tiempo de ejecución 1")
+console.timeEnd("Tiempo de ejecución 1");
 
 // assert
 
-let age = 17
-console.assert(age >= 18, "El usuario debe ser mayor de edad.")
+let age = 17;
+console.assert(age >= 18, "El usuario debe ser mayor de edad.");
 
 // count
 
-console.count("Click")
-console.count("Click")
-console.count("Click")
-console.countReset("Click")
-console.count("Click")
+console.count("Click");
+console.count("Click");
+console.count("Click");
+console.countReset("Click");
+console.count("Click");
 
 // trace
 
 function funcA() {
-    funcB()
+  funcB();
 }
 
 function funcB() {
-    console.trace("Seguimiento de la ejecución.")
+  console.trace("Seguimiento de la ejecución.");
 }
 
-funcA()
+funcA();
 
 // clear
 
--- Basic/27-console-methods-exercises.js
+++ Basic/27-console-methods-exercises.js
@@ -4,21 +4,116 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=21421
 */
 
 // 1. Crea un función que utilice error correctamente
+console.log("\n1. Crea un función que utilice error correctamente");
+function dividir(a, b) {
+  if (b === 0) {
+    throw new Error("No se puede dividir por cero.");
+  }
+  return a / b;
+}
+
+try {
+  const resultado = dividir(10, 2);
+  console.log("Resultado:", resultado);
+
+  const error = dividir(10, 0);
+  console.log(error);
+} catch (error) {
+  console.log("error:", error.message);
+}
 
 // 2. Crea una función que utilice warn correctamente
+console.log("\n2. Crea una función que utilice warn correctamente");
+
+function verificarStock(producto, cantidad) {
+  if (cantidad < 5) {
+    console.warn(`advertencia: quedan ${cantidad} unidades de ${producto}`);
+  }
+  return `stock de ${producto} :${cantidad} unidades`;
+}
+
+console.log(verificarStock("manzanas", 3));
+console.log(verificarStock("naranjas", 10));
 
 // 3. Crea una función que utilice info correctamente
+console.log("\n3. Crea una función que utilice info correctamente");
+function iniciarSesion(usuario) {
+  console.info(`Información: el usuario ${usuario} inició sesión`);
+
+  return `Bienvenido ${usuario}`;
+}
+console.log(iniciarSesion("toño"));
 
 // 4. Utiliza table
-
+console.log("\n4. Utiliza table");
+const usuarios = [
+  { nombre: "toño", edad: 39, pais: "colombia" },
+  { nombre: "maria", edad: 25, pais: "mexico" },
+  { nombre: "luis", edad: 30, pais: "españa" },
+];
+console.table(usuarios);
 // 5. Utiliza group
+console.log("\n5. Utiliza group");
+
+console.group("Información de usuarios");
+console.log("nombre: Toño");
+console.log("edad: 39");
+console.log("pais: colombia");
+console.groupEnd();
 
 // 6. Utiliza time
+console.log("\n6. Utiliza time");
+
+console.time("tiempo de ejecución");
+for (let i = 0; i < 1000000; i++) {
+  // Simulación de una tarea pesada
+}
+console.timeEnd("tiempo de ejecución");
 
 // 7. Valida con assert si un número es positivo
+console.log("\n7. Valida con assert si un número es positivo");
+
+function validarNumero(numero) {
+  console.assert(numero > 0, `Error: el numero ${numero} no es positivo`);
+  console.log(`numero recibido: ${numero}`);
+}
+validarNumero(5);
+validarNumero(-3);
 
 // 8. Utiliza count
+console.log("\n8. Utiliza count");
+function saludar(nombre) {
+  console.count("cantidad de saludos");
+  console.log(`hola, ${nombre}`);
+}
+saludar("toño");
+saludar("maria");
+saludar("dany");
+saludar("luis");
+saludar("maria");
 
 // 9. Utiliza trace
+console.log("\n9. Utiliza trace");
+function funcionA() {
+  funcionB();
+}
+function funcionB() {
+  funcionC();
+}
+
+function funcionC() {
+  console.trace("traza de funciones");
+}
+
+funcionA();
 
 // 10. Utiliza clear
+console.log("\n10. Utiliza clear");
+
+console.log("mensaje1");
+console.log("mensaje2");
+console.log("mensaje3");
+
+//console.clear();
+
+console.log("consola limpia");
